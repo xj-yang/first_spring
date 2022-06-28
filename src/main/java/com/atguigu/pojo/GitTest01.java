435167fa0c9c184a6b967c857cf6544b6bbc660f
tom_glb <goodMorning@yjq.com>
2022-06-28 18:52:10 +0800

    dev 分支修改了GitTest01文件

---
+++ src/main/java/com/atguigu/pojo/GitTest01.java
@@ -0,0 +1,12 @@
+package com.atguigu.pojo;
+
+/**
+ * @author yjq
+ * @create 2022-06-28-18:51
+ */
+public class GitTest01 {
+
+    public static void main(String[] args) {
+        System.out.println("this is dev modify...");
+    }
+}
