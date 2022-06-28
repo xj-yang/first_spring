c433630ddc0aa8a1210ca28b1444ac7c38ef7abe
tom_glb <goodMorning@yjq.com>
2022-06-28 18:50:12 +0800

    hot_fix分支修改了GitTest01文件

---
+++ src/main/java/com/atguigu/pojo/GitTest01.java
@@ -0,0 +1,12 @@
+package com.atguigu.pojo;
+
+/**
+ * @author yjq
+ * @create 2022-06-28-18:48
+ */
+public class GitTest01 {
+
+    public static void main(String[] args) {
+        System.out.println("this  is hot_fix modify....");
+    }
+}
